47535ba200819158e8162baa906714b4a894fa2a
raavi-kaur <ravinderkaur1709@gmail.com>
2026-03-12 17:34:03 +0530

    subnet-2 added

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "test-vpc"
+  }
 }
 resource "aws_subnet" "example_subnet" {
   vpc_id            = aws_vpc.example.id
@@ -8,5 +12,14 @@ resource "aws_subnet" "example_subnet" {
 
   tags = {
     Name = "subnet-1"
+  }
+}
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet-2"
   }
 }
