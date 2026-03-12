40590a16c4c4c21f5ef29dacbde6ac3cd69a5911
raavi-kaur <ravinderkaur1709@gmail.com>
2026-03-12 17:25:12 +0530

    subnet-1 added

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
